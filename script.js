3acb47721af56012486fb53eb35c5523e39569dc
macabanto <antmacapple@gmail.com>
2024-12-09 01:34:22 +1100

    dvd logo rotation rotates about z axis

--- script.js
+++ script.js
@@ -16,52 +16,93 @@ let dvd_color_arr =
     ]
 var current_color_position = 0;
 
+let dvd_div = document.getElementById("dvd_div");
+let dvd_svg = document.getElementById("dvd_svg");
+
+var position_direction_x = 1;
+var position_direction_y = 1;
+var rotation_direction_z = 1;//x axis points normal to display
+
+var window_size_x = window.innerWidth;
+var window_size_y = window.innerHeight;
+
+var position_x = Math.random() * (window_size_x - dvd_div.offsetWidth); // Initial x position
+var position_y = Math.random() * (window_size_y - dvd_div.offsetHeight); // Initial y position
+var rotation_z = 0; // Initial x rotation is 0
+
+// Generate random deltas for movement
+let position_delta_x = Math.random() * 4 + 1; // Values range from 1 ~ 5
+let position_delta_y = Math.random() * 4 + 1; // Values range from 1 ~ 5
+let rotation_delta_z = Math.random() * 2 + 1;// 5 ~ 15; rotates about x axis ( spinning )
+
 function start_game() {//start button clicked
-    setInterval(translate_dvd_div, 17); // Applies translation every 10ms
+    dvd_div.style.transform = `translate(${position_x}px, ${position_y}px)`;//moves dvd logo to position
+    setInterval(apply_transform, 17); // Applies transform every 10ms
+    change_color();
     start_div.classList.replace("visible", "invisible");
     dvd_div.classList.replace("invisible", "visible");
-    change_color();
 }
 // DVD
 
-let dvd_div = document.getElementById("dvd_div");
-let dvd_svg = document.getElementById("dvd_svg");
-let svg_path = document.getElementById("svg_path");
-
-var direction_x = 1;
-var direction_y = 1;
-var window_x_max = window.innerWidth;
-var window_y_max = window.innerHeight;
-let posX = Math.random() * (window_x_max - dvd_div.offsetWidth); // Initial x position
-let posY = Math.random() * (window_y_max - dvd_div.offsetHeight); // Initial y position
-// Generate random deltas for movement
-let delta_x = Math.random() * 4 + 1; // Values range from 1 to 5
-let delta_y = Math.random() * 4 + 1; // Values range from 1 to 5
-
-function change_color() {
+function change_color() {//cycle colours
     current_color_position = current_color_position+1 == dvd_color_arr.length ? 0 : current_color_position + 1;
     svg_path.setAttribute("fill", dvd_color_arr[current_color_position]);
 }
 
-function translate_dvd_div() {
-    // Update the position
-    
-    posX += delta_x * direction_x;
-    posY += delta_y * direction_y;
-
-    // Window boundary
-    var top_boundary = posY <= 0;
-    var bottom_boundary = posY + dvd_div.offsetHeight >= window_y_max;
-    var left_boundary = posX <= 0;
-    var right_boundary = posX + dvd_div.offsetWidth >= window_x_max;
-
+function is_corner(){//determines if dvd logo is in corner, returns true / false if not
     // Corners
+    var top_boundary = position_y <= 0;
+    var bottom_boundary = position_y + dvd_div.offsetHeight >= window_size_y;
+    var left_boundary = position_x <= 0;
+    var right_boundary = position_x + dvd_div.offsetWidth >= window_size_x;
+
     var top_left_corner = top_boundary && left_boundary;
     var top_right_corner = top_boundary && right_boundary;
     var bot_left_corner = bottom_boundary && left_boundary;
     var bot_right_corner = bottom_boundary && right_boundary;
-
     if (top_left_corner || top_right_corner || bot_left_corner || bot_right_corner) {
+        return true;
+    }
+    return false;
+}
+
+function is_boundary(){//determines if dvd logo has met boundary, returns true / false if not
+    // Window boundary
+    var top_boundary = position_y <= 0;
+    var bottom_boundary = position_y + dvd_div.offsetHeight >= window_size_y;
+    var left_boundary = position_x <= 0;
+    var right_boundary = position_x + dvd_div.offsetWidth >= window_size_x;
+    if (top_boundary || bottom_boundary || left_boundary || right_boundary) {
+        return true;
+    }
+    return false;
+}
+
+function rotate_dvd_div(){//calculates rotation tranform, returns transform as string
+    rotation_z += ( rotation_delta_z * rotation_direction_z );
+    if (is_boundary()) {//dvd logo hits wall
+        // Change 
+        rotation_direction_z *= -1;
+    } 
+    console.log(dvd_div.offsetHeight);//as div rotates, height should change no?
+    console.log(dvd_div.getBoundingClientRect()['top']);//as div rotates, height should change no?
+
+    return `rotateZ(${rotation_z}deg)`;
+}
+
+function translate_dvd_div() {//calculates translation tranform, returns transform as string
+    // Update the position
+    
+    position_x += ( position_delta_x * position_direction_x );
+    position_y += ( position_delta_y * position_direction_y );
+
+    // Window boundary
+    var top_boundary = position_y <= 0;
+    var bottom_boundary = position_y + dvd_div.offsetHeight >= window_size_y;
+    var left_boundary = position_x <= 0;
+    var right_boundary = position_x + dvd_div.offsetWidth >= window_size_x;
+
+    if (is_corner()) {
         // Corner *reached*... now what...
         dvd_svg.style.transform = 'scale(2, 2)'; // Scale width and height by 200%
         console.log("corner reached!");
@@ -71,28 +112,29 @@ function translate_dvd_div() {
     } else {
         if (top_boundary) { // Top boundary reached, go down
             change_color();
-            direction_y = 1;
+            position_direction_y = 1;
             
         }
         if (bottom_boundary) { // Bottom boundary reached, go up
             change_color();
-            direction_y = -1;
+            position_direction_y = -1;
             
         }
         if (left_boundary) { // Left boundary reached, go right
             change_color();
-            direction_x = 1;
+            position_direction_x = 1;
             
         }
         if (right_boundary) { // Right boundary reached, go left
             change_color();
-            direction_x = -1;
+            position_direction_x = -1;
             
         }
     }
-
     // Apply the CSS transform
-    dvd_div.style.transform = `translate(${posX}px, ${posY}px)`;
-    // Log the new positions
-    //console.log("Translated x: " + posX + "\nTranslated y: " + posY);
+    return `translate(${position_x}px, ${position_y}px)`;
+}
+
+function apply_transform(){//combines tranforms into single string, returns string
+    dvd_div.style.transform = "".concat(translate_dvd_div()," ",rotate_dvd_div());
 }
