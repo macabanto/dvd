9929d23994d85a643f2af5966d3514c1822c5836
macabanto <antmacapple@gmail.com>
2024-12-14 12:56:43 +1100

    establishing branch for basic functionality

--- script.js
+++ script.js
@@ -2,97 +2,115 @@
 let start_div = document.getElementById("start_div");
 // array stores paths for dvd.svg files: use this to alternate between colors
 let dvd_color_arr =
-    [
-        "#f54242",
-        "#f58442",
-        "#f5d142",
-        "#72f542",
-        "#42f581",
-        "#42f5e9",
-        "#428af5",
-        "#8a42f5",
-        "#ef42f5",
-        "#f54260",
-    ]
+[
+    "#f54242",
+    "#f58442",
+    "#f5d142",
+    "#72f542",
+    "#42f581",
+    "#42f5e9",
+    "#428af5",
+    "#8a42f5",
+    "#ef42f5",
+    "#f54260",
+]
 var current_color_position = 0;
 
-function start_game() {//start button clicked
-    setInterval(translate_dvd_div, 17); // Applies translation every 10ms
-    start_div.classList.replace("visible", "invisible");
-    dvd_div.classList.replace("invisible", "visible");
-    change_color();
-}
-// DVD
-
-let dvd_div = document.getElementById("dvd_div");
+let play_div = document.getElementById("play_div");
 let dvd_svg = document.getElementById("dvd_svg");
-let svg_path = document.getElementById("svg_path");
 
-var direction_x = 1;
-var direction_y = 1;
-var window_x_max = window.innerWidth;
-var window_y_max = window.innerHeight;
-let posX = Math.random() * (window_x_max - dvd_div.offsetWidth); // Initial x position
-let posY = Math.random() * (window_y_max - dvd_div.offsetHeight); // Initial y position
-// Generate random deltas for movement
-let delta_x = Math.random() * 4 + 1; // Values range from 1 to 5
-let delta_y = Math.random() * 4 + 1; // Values range from 1 to 5
+var position_direction_x = 1;
+var position_direction_y = 1;
+var rotation_direction_z = 1;//x axis points normal to display
 
-function change_color() {
+var window_size_x = 0;
+var window_size_y = 0;
+
+var position_x = 0; // Initial x position
+var position_y = 0; // Initial y position
+
+var position_delta_x = Math.random() * 3 + 2; // Values range from 3-5
+var position_delta_y = Math.random() * 3 + 2; // Values range from 3-5
+
+window.onload = () => {
+    document.getElementById("start_button").addEventListener("click", start_game);
+    window.addEventListener('keydown', function(event){
+        if(event.key === " "){
+            start_game();
+        }
+    })
+};
+
+// GAME
+var boundary = 0;
+var bounces=document.getElementById('bounces');
+
+function start_game() {//applies correct css  making start menu invisible, game visible
+    
+    window_size_x = window.innerWidth;//setting window size 
+    window_size_y = window.innerHeight; //setting window size 
+
+    setInterval(transform_svg, 16.666666666); // Applies transform every 50ms ( 60fps )
+
+    position_x = 100; // Initial x position
+    position_y = 100; // Initial y position
+    
+    change_color();
+    dvd_svg.style.transform = `translate(${position_x}px, ${position_y}px)`;//moves dvd logo to position
+    start_div.classList.replace("visible", "invisible");
+    play_div.classList.replace("invisible", "visible");
+}
+
+function change_color() {//cycle colours
     current_color_position = current_color_position+1 == dvd_color_arr.length ? 0 : current_color_position + 1;
     svg_path.setAttribute("fill", dvd_color_arr[current_color_position]);
 }
 
-function translate_dvd_div() {
+function transform_svg() {//calculates and applies transform to div
     // Update the position
-    
-    posX += delta_x * direction_x;
-    posY += delta_y * direction_y;
-
-    // Window boundary
-    var top_boundary = posY <= 0;
-    var bottom_boundary = posY + dvd_div.offsetHeight >= window_y_max;
-    var left_boundary = posX <= 0;
-    var right_boundary = posX + dvd_div.offsetWidth >= window_x_max;
-
-    // Corners
-    var top_left_corner = top_boundary && left_boundary;
-    var top_right_corner = top_boundary && right_boundary;
-    var bot_left_corner = bottom_boundary && left_boundary;
-    var bot_right_corner = bottom_boundary && right_boundary;
-
-    if (top_left_corner || top_right_corner || bot_left_corner || bot_right_corner) {
-        // Corner *reached*... now what...
-        dvd_svg.style.transform = 'scale(2, 2)'; // Scale width and height by 200%
-        console.log("corner reached!");
-        // Change both x and y direction
-        direction_x *= -1;
-        direction_y *= -1;
-    } else {
+    let logo_boundary = dvd_svg.getBoundingClientRect();//getting svg location
+    // boundary booleans
+    var top_boundary = logo_boundary['top'] <= 0;
+    var bottom_boundary = logo_boundary['bottom'] >= window_size_y;
+    var right_boundary = logo_boundary['right'] >= window_size_x;
+    var left_boundary = logo_boundary['left'] <= 0;
+    boundary = 0;
         if (top_boundary) { // Top boundary reached, go down
+            rotation_direction_z *= -1;
             change_color();
-            direction_y = 1;
-            
+            position_direction_y = 1;
+            let bounce_value = parseInt(bounces.innerText);
+            bounces.innerText = ( bounce_value + 1 );
+            boundary = 1;
         }
         if (bottom_boundary) { // Bottom boundary reached, go up
+            rotation_direction_z *= -1;
             change_color();
-            direction_y = -1;
-            
+            position_direction_y = -1;
+            let bounce_value = parseInt(bounces.innerText);
+            bounces.innerText = ( bounce_value + 1 );
+            boundary = 1;
         }
         if (left_boundary) { // Left boundary reached, go right
+            rotation_direction_z *= -1;
             change_color();
-            direction_x = 1;
-            
+            position_direction_x = 1;
+            let bounce_value = parseInt(bounces.innerText);
+            bounces.innerText = ( bounce_value + 1 );
+            boundary = 1;
         }
         if (right_boundary) { // Right boundary reached, go left
+            rotation_direction_z *= -1;
             change_color();
-            direction_x = -1;
-            
+            position_direction_x = -1;
+            let bounce_value = parseInt(bounces.innerText);
+            bounces.innerText = ( bounce_value + 1 );
+            boundary = 1;
         }
-    }
 
+        position_x += ( position_delta_x * position_direction_x );
+        position_y += ( position_delta_y * position_direction_y );
+    
     // Apply the CSS transform
-    dvd_div.style.transform = `translate(${posX}px, ${posY}px)`;
-    // Log the new positions
-    //console.log("Translated x: " + posX + "\nTranslated y: " + posY);
+    dvd_svg.style.transform = `translate(${position_x}px, ${position_y}px)`;
 }
